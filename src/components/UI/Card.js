eaf2284c112c0cbed0e9a65278c9e5f00ad83710
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-07-16 19:10:56 -0500

    card css

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,6 +3,7 @@ import classes from './Card.module.css';
 
 
 const Card = (props) => {
+    // whatever name you're using, you have to use as the props.className. if it is called cssName then you need to make it called that
      return (
          <div className={`${classes.card} ${props.className}`}>{props.children}</div>
      )
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
-
+import Button from '../UI/Button';
 const AddUser = (props) => {
+    const [enteredUser, setEnteredUser] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUser, enteredAge);
+        setEnteredUser('');
+        setEnteredAge('');
     };
 
+    const userNameChangeHandler = (event) => {
+        setEnteredUser(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
     return (
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
+            <input id="username" type="text" onChange={userNameChangeHandler}></input>
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
+            <input id="age" type="number" onChange={ageChangeHandler}></input>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     );
