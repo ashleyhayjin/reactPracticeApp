0d08704ae1f4f2758278da6d18a55a7e3d8b3cb3
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-07-17 20:06:01 -0500

    functions

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -8,6 +8,13 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        if(enteredUser.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+            // return finishes the function execution 
+        }
+        if(+enteredAge < 1){
+            return;
+        }
         console.log(enteredUser, enteredAge);
         setEnteredUser('');
         setEnteredAge('');
@@ -23,9 +30,18 @@ const AddUser = (props) => {
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" onChange={userNameChangeHandler}></input>
+            <input 
+                id="username" 
+                type="text" 
+                onChange={userNameChangeHandler}
+
+            />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" onChange={ageChangeHandler}></input>
+            <input 
+                id="age" 
+                type="number" 
+                onChange={ageChangeHandler}
+            />
             <Button type="submit">Add User</Button>
         </form>
         </Card>
